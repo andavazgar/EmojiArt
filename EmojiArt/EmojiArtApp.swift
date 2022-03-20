8b7ad11a4c99155c7e6e0a10be7b372908e76f49
Andres Vazquez <andavazgar.dev@gmail.com>
2022-03-19 22:14:29 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Andres Vazquez on 2022-03-19.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
